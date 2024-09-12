e39cb0c37f311f9cc62a9437d0447e1e3517cb0e
Yana <yanaegorova69.3@gmail.com>
2024-09-13 02:49:01 +0300

    template created

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import First from '../First/First';
+import Second from '../Second/Second';
+
+function App() {
+
+
+  return (
+    <>
+      <First />
+      <Second />
+    </>
+  )
+}
+
+export default App
